36ff48f1a3afc35748f849859549dc643c83fa1c
Tariq Hook <thook@Tariqs-MacBook-Pro-2.local>
2022-02-21 10:50:43 -0500

    Examples

--- src/main/java/com/codedifferentlty/Solution.java
+++ src/main/java/com/codedifferentlty/Solution.java
@@ -1,5 +1,8 @@
 package com.codedifferentlty;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class Solution {
 
     /**
@@ -12,6 +15,53 @@ public class Solution {
      * because this string contains only 9 distinct characters.
      */
     public static Boolean uniqueCharacters(String input){
-        return null;
+        HashSet<Character> characters = new HashSet<>();
+        for (int x=0 ; x < input.length(); x++)
+        {
+            Character character = input.charAt(x);
+            if (characters.contains(character)){
+                return false;
+            }
+            characters.add(character);
+        }
+        return true;
     }
+
+    public static Boolean uniqueCharactersV2(String input){
+        StringBuilder builder = new StringBuilder();
+        for(int x =0; x < input.length(); x++){
+            Character character = input.charAt(x);
+            if(builder.indexOf(String.valueOf(character)) != -1){
+                return false;
+            }
+            builder.append(character);
+        }
+        return true;
+    }
+
+    public static Boolean uniqueCharactersV3(String input){
+        char[] characters = input.toCharArray();
+        Arrays.sort(characters);
+        for (int x=1; x < characters.length; x++){
+           if (characters[x] == characters[x-1]){
+               return false;
+           }
+        }
+        return true;
+    }
+
+    public static Boolean uniqueCharactersV4(String input){
+        for(int x = 0; x < input.length(); x++){
+            Character character = input.charAt(x);
+            for(int y= x+1; y < input.length(); y++){
+                Character compare = input.charAt(y);
+                if (character == compare){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
 }
